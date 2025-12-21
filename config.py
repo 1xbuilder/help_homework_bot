5cbdfcc3d3804bfba8fb265c008c81ac706dfe50
1xbuilder <132667670+1xbuilder@users.noreply.github.com>
2025-12-22 01:46:30 +0600

    Add files via upload

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 TOKEN = "6029878031:AAEzDzfx7dVmZmGcA-sy5B2c1SqYSR-bRks"
+SUPABASE_URL = "https://cpppvkrvduhdvgutibec.supabase.co"
+SUPABASE_KEY = "sb_publishable_eekjhCteBrBNZsd0MPSGuQ_gUnyHrrZ"
 # ADMIN_IDS = [767496462, 1148528051, 5350177621, 2112224274, 7289021581]

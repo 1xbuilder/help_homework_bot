3c0f76d07cb00de1a1acb85d382cee487d9e334e
1xbuilder <132667670+1xbuilder@users.noreply.github.com>
2026-03-29 12:42:39 +0600

    Delete help_homework_bot-fixed directory

--- help_homework_bot-fixed/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-TOKEN = os.getenv("BOT_TOKEN") or os.getenv("TOKEN") or os.getenv("API_TOKEN", "")
-SUPABASE_URL = os.getenv("SUPABASE_URL", "")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY", "")
-
-_raw_ids = os.getenv("ADMIN_IDS", "")
-ADMIN_IDS = [int(x.strip()) for x in _raw_ids.split(",") if x.strip().isdigit()]
